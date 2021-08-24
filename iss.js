74141096dfb925ec60ceee4464e9b0253a05808b
Justin Lam <justin.kh.lam@gmail.com>
2021-08-24 02:40:17 +0000

    add fetchCoordsByIP

--- iss.js
+++ iss.js
@@ -12,7 +12,7 @@ const request = require('request');
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
 
-  request('https://api.ipify.org?format=json', (error, response, body) => {
+  request(`https://api.ipify.org?format=json`, (error, response, body) => {
 
     if (error) { // print error if not null
       callback(`error: ${error}`, null); // error/null for desc
@@ -29,4 +29,25 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function (ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+
+    if (error) { // print error if not null
+      callback(`error: ${error}`, null); // error/null for desc
+      return;
+    }
+
+    if (response.statusCode !== 200) { // print status code if not successful
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+
+}
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
